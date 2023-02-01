3240177d82b5a13859ccdbc119cd3bee5371b5ee
amrlhzmii <123861037+amrlhzmii@users.noreply.github.com>
2023-02-01 23:17:02 +0800

    Add files via upload

---
+++ main EDITED.cpp
@@ -0,0 +1,22 @@
+// ********************************************************* 
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS 
+// Year: Trimester 1, 2022/23 (T2215) 
+// Lab: TxxL 
+// Names: AMIRUL_HAZMI | MUHAMMAD_ZAQIR_BIN_ZILKIFLI| MEMBER_NAME_3 
+// IDs: 1221303899 | 1221304046 | MEMBER_ID_3 
+// Emails: 1221303899@student.mmu.edu.my | 1221304046@student.mmu.edu.my | MEMBER_EMAIL_3 
+// Phones: 0126556492 | 01135657702 | MEMBER_PHONE_3 
+// ********************************************************* 
+
+// TODO: Fill in the missing information above and delete this line.
+
+#include "pf/helper.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Assignment (Part 1)" << endl;
+    cout << "Let's Get Started!" << endl;
+    pf::Pause();
+}
